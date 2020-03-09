9381e0158f603f2b9ff304205957a49ff36ea7ae
harmonious-cache-6099 <isaac_scothern@hotmail.co.uk>
2020-03-09 20:27:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,33 @@
-var kittens = [] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
 // Add your functions and code here
+function destructivelyAppendKitten(name) {
+   return kittens.push(name);
+
+}
+
+function destructivelyPrependKitten(name) {
+  return kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten(name){
+  return kittens.splice(kittens.length - 1, 1);
+}
+
+function  destructivelyRemoveFirstKitten(name) {
+  return kittens.splice(0, 1);
+}
+
+function appendKitten(name){
+  var newArray = [...kittens, name];
+  return newArray;
+}
+
+function  prependKitten(name) {
+  var newArray = [name, ...kittens];
+  return newArray;
+}
+
+function removeLastKitten() {
+
+}
